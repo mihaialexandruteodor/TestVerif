9766aa63c0dbf112fd6aa340a2d42715e2024f64
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-04-02 16:00:38 +0300

    indiv proj

---
+++ IndivProj/ATM/test/BankDatabaseTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankDatabaseTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAccount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void authenticateUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAvailableBalance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTotalBalance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void credit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void debit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getadmin() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getaccpin() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void adduser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteuser() {
+    }
+}
