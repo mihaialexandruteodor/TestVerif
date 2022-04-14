1b8d71cd037a58c6ef2db646b649dd916afc3792
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-04-14 11:10:31 +0300

    EQUIVALENCE PARTITIONING

--- IndivProj/ATM/test/BankDatabaseTest.java
+++ IndivProj/ATM/test/BankDatabaseTest.java
@@ -1,4 +1,7 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BankDatabaseTest {
 
@@ -7,14 +10,17 @@ class BankDatabaseTest {
     Account accounts2;
     Account accounts3;
     Account accounts4;
+    Account[] accounts;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         bd = new BankDatabase();
         accounts1 = new Account("Customer1", 12345, 11111, 1000.0, 1200.0, 0);
-        accounts2 = new Account("Customer2", 98765, 22222, 200.0, 200.0, 0);
-        accounts3 = new Account("Customer3", 19234, 33333, 200.0, 200.0, 0);
+        accounts2 = new Account("Customer3", 19234, 33333, 200.0, 200.0, 0);
+        accounts3 = new Account("Customer2", 98765, 22222, 200.0, 200.0, 0);
         accounts4 = new Account("Manager1", 99999, 00000, 0, 0, 1);
+
+        accounts = new Account[]{accounts1, accounts2, accounts3, accounts4};
     }
 
     @org.junit.jupiter.api.AfterEach
@@ -28,15 +34,31 @@ class BankDatabaseTest {
 
     @org.junit.jupiter.api.Test
     void getAccount() {
-        assertTrue(bd.getAccount(12345).getUsername().equals(accounts1.getUsername()));
-        assertEquals(bd.getAccount(98765).getTotalBalance(),accounts2.getTotalBalance());
-        assertEquals(bd.getAccount(98765).getAvailableBalance(),accounts2.getAvailableBalance());
-        assertEquals(bd.getAccount(19234).getPin(),accounts3.getPin());
-        assertEquals(bd.getAccount(99999).getISadmin(),accounts4.getISadmin());
+
+        // EQUIVALENCE PARTITIONING
+
+        int[] partitioning = {12345, 19234, 98765, 99999, 100000};
+
+        for(int i=0; i < 4; ++i)
+        {
+            int bound = ThreadLocalRandom.current().nextInt(partitioning[i] , partitioning[i+1] + 1);
+
+            assertTrue(accounts[i].getAccountNumber() <= bound);
+        }
+
+
+
+
+
     }
 
     @org.junit.jupiter.api.Test
     void authenticateUser() {
+        assertTrue(bd.getAccount(12345).getUsername().equals(accounts1.getUsername()));
+        assertEquals(bd.getAccount(98765).getTotalBalance(),accounts3.getTotalBalance());
+        assertEquals(bd.getAccount(98765).getAvailableBalance(),accounts3.getAvailableBalance());
+        assertEquals(bd.getAccount(19234).getPin(),accounts2.getPin());
+        assertEquals(bd.getAccount(99999).getISadmin(),accounts4.getISadmin());
     }
 
     @org.junit.jupiter.api.Test
@@ -61,6 +83,7 @@ class BankDatabaseTest {
 
     @org.junit.jupiter.api.Test
     void getaccpin() {
+
     }
 
     @org.junit.jupiter.api.Test
