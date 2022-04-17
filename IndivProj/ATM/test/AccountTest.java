f98b302e818a0fde36380d04d2829361033af99d
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-04-17 17:06:44 +0300

    accounts

--- IndivProj/ATM/test/AccountTest.java
+++ IndivProj/ATM/test/AccountTest.java
@@ -52,56 +52,66 @@ class AccountTest {
 
     }
 
-    @Test
-    void credit() {
-    }
-
-    @Test
-    void debit() {
-    }
 
     @Test
     void getAccountNumber() {
+        assertEquals(accounts[1].getAccountNumber(), 98765);
     }
 
     @Test
     void getISadmin() {
+        assertEquals(accounts[3].getISadmin(),1);
     }
 
     @Test
     void getPin() {
+        assertEquals(accounts[0].getPin(),11111);
     }
 
     @Test
     void getUsername() {
+        assertEquals(accounts[3].getUsername(),"Manager1");
     }
 
     @Test
     void setUsername() {
+        accounts[3].setUsername("Different");
+        assertEquals(accounts[3].getUsername(),"Different");
     }
 
     @Test
     void setAccountNumber() {
+        accounts[1].setAccountNumber(88888);
+        assertEquals(accounts[1].getAccountNumber(), 88888);
     }
 
 
     @Test
     void setPin() {
+        accounts[0].setPin(8976543);
+        assertEquals(accounts[0].getPin(),8976543);
     }
 
     @Test
     void setAvailableBalance() {
+        accounts[1].setAvailableBalance(12345.0);
+        assertEquals(accounts[1].getAvailableBalance(),12345.0);
     }
 
     @Test
     void setTotalBalance() {
+        accounts[1].setTotalBalance(5555.0);
+        assertEquals(accounts[1].getTotalBalance(),5555.0);
     }
 
     @Test
     void getAdmin() {
+        assertEquals(accounts[1].getAdmin(), 0);
     }
 
     @Test
     void setAdmin() {
+        accounts[1].setAdmin(1);
+        assertEquals(accounts[1].getAdmin(), 1);
     }
 }
