aac33184fc9697cb4a3e649c147a2b8cc275cd4c
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-04-17 22:17:11 +0300

    transaction

--- IndivProj/ATM/test/TransactionTest.java
+++ IndivProj/ATM/test/TransactionTest.java
@@ -6,23 +6,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
 
+    Transaction transaction;
+    BankDatabase bd;
+    Screen screen;
+
     @BeforeEach
     void setUp() {
+        screen = new Screen();
+        bd = new BankDatabase();
+        transaction = new Transaction(12345, screen, bd) {
+            @Override
+            public void execute() {
+            }
+        };
     }
 
     @AfterEach
     void tearDown() {
+        screen = null;
+        bd = null;
+        transaction = null;
     }
 
     @Test
     void getAccountNumber() {
+        assertEquals(transaction.getAccountNumber(), 12345);
     }
 
     @Test
     void getScreen() {
+        assertEquals(transaction.getScreen(), screen);
     }
 
     @Test
     void getBankDatabase() {
+        assertEquals(transaction.getBankDatabase(), bd);
     }
 }
